aaecf9dafbae6ecd0409f087e9d0054de2d2ffda
Pran Kishore <kishorepran@gmail.com>
2018-03-27 14:11:37 +0200

    Created a cocoa touch framework. Included the utilities file inside it + unite test target to ensure the stability of application.

---
+++ Utilities/Utilities.h
@@ -0,0 +1,19 @@
+//
+//  Utilities.h
+//  Utilities
+//
+//  Created by Pran Kishore on 27/03/18.
+//  Copyright © 2018 Pran Kishore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Utilities.
+FOUNDATION_EXPORT double UtilitiesVersionNumber;
+
+//! Project version string for Utilities.
+FOUNDATION_EXPORT const unsigned char UtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Utilities/PublicHeader.h>
+
+
---
+++ UtilitiesTests/UtilitiesTests.m
@@ -0,0 +1,36 @@
+//
+//  UtilitiesTests.m
+//  UtilitiesTests
+//
+//  Created by Pran Kishore on 27/03/18.
+//  Copyright © 2018 Pran Kishore. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "NSDate+Utilities.h"
+@interface UtilitiesTests : XCTestCase
+
+@end
+
+@implementation UtilitiesTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    NSDate * today = [NSDate dateYesterday];
+    XCTAssertTrue(today.isToday);
+    
+}
+
+@end
